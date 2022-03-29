966e1b1b7485892b584b89a60a24f5064a6b2d9a
agil20 <agilakh@code.edu.az>
2022-03-29 18:12:34 +0400

    bugh fixed: I fixed Error in proqram cs

--- Static .Interface/Static .Interface/Program.cs
+++ Static .Interface/Static .Interface/Program.cs
@@ -6,24 +6,7 @@ namespace Static_.Interface
     {
         static void Main(string[] args)
         {
-            do
-            {
-
-              Console.WriteLine("Daxil edin   ");
-            Console.WriteLine("Email  ");
-
-            User.Email= Console.ReadLine();
-            Console.WriteLine("Password  ");
-            User.Password= Console.ReadLine();    
            
-            Console.WriteLine("Ad");
-            User.Fullname = Console.ReadLine();
-                //Id dəyəri hər dəfə bir user obyekti yaranan zaman avtomatik artmalıdır 
-                User.ShowInfo();
-                
-                
-            } while (true);
-
 
 
         }
